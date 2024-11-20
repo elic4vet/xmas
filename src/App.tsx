30153c3a1b4618f35bf3f8e3c11a70a277c3c65a
Elisabeth <elise.doisneau@gmail.com>
2024-11-20 20:30:04 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <section className='first_row'>
+          <div className="row" id='one'> 1 </div>
+        </section>
+        <section className='second_row'>
+          <div className="row" id='two'> 2 </div>
+          <div className="row" id='three'> 3 </div>
+        </section>
+        <section className='third_row'>
+          <div className="row" id='four'> 4 </div>
+          <div className="row" id='five'> 5 </div>
+          <div className="row" id='six'> 6 </div>
+        </section>
+        <section className='fourth_row'>
+          <div className="row" id='seven'> 7 </div>
+          <div className="row" id='eight'> 8 </div>
+          <div className="row" id='nine'> 9 </div>
+          <div className="row" id='ten'> 10 </div>
+        </section>
+        <section className='fifth_row'>
+          <div className="row" id='eleven'> 11 </div>
+          <div className="row" id='twelve'> 12 </div>
+          <div className="row" id='thirteen'> 13 </div>
+          <div className="row" id='fourteen'> 14 </div>
+          <div className="row" id='fifteen'> 15 </div>
+          <div className="row" id='sixteen'> 16 </div>
+
+        </section>
+        <section className='sixth_row'>
+          <div className="row" id='seventeen'> 17 </div>
+          <div className="row" id='eighteen'> 18 </div>
+          <div className="row" id='nineteen'> 19 </div>
+          <div className="row" id='twenty'> 20 </div>
+          <div className="row" id='twenty-one'> 21 </div>
+          <div className="row" id='twenty-two'> 22 </div>
+          <div className="row" id='twenty-three'> 23 </div>
+        </section>
+
+        <section className='seventh_row'>
+ 
+          <div className="row" id='twenty-four'> 24 </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
