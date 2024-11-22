af63fc3aaaf00056a7c3834d069444988e033f19
Elisabeth <elise.doisneau@gmail.com>
2024-11-22 20:10:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,45 +8,45 @@ function App() {
     <>
       <div>
         <section className='first_row'>
-          <div className="row" id='one'> 1 </div>
+          <div id='one'> 1 </div>
         </section>
         <section className='second_row'>
-          <div className="row" id='two'> 2 </div>
-          <div className="row" id='three'> 3 </div>
+          <div id='two'> 2 </div>
+          <div id='three'> 3 </div>
         </section>
         <section className='third_row'>
-          <div className="row" id='four'> 4 </div>
-          <div className="row" id='five'> 5 </div>
-          <div className="row" id='six'> 6 </div>
+          <div id='four'> 4 </div>
+          <div id='five'> 5 </div>
+          <div id='six'> 6 </div>
         </section>
         <section className='fourth_row'>
-          <div className="row" id='seven'> 7 </div>
-          <div className="row" id='eight'> 8 </div>
-          <div className="row" id='nine'> 9 </div>
-          <div className="row" id='ten'> 10 </div>
+          <div id='seven'> 7 </div>
+          <div id='eight'> 8 </div>
+          <div id='nine'> 9 </div>
+          <div id='ten'> 10 </div>
         </section>
         <section className='fifth_row'>
-          <div className="row" id='eleven'> 11 </div>
-          <div className="row" id='twelve'> 12 </div>
-          <div className="row" id='thirteen'> 13 </div>
-          <div className="row" id='fourteen'> 14 </div>
-          <div className="row" id='fifteen'> 15 </div>
-          <div className="row" id='sixteen'> 16 </div>
+          <div id='eleven'> 11 </div>
+          <div id='twelve'> 12 </div>
+          <div id='thirteen'> 13 </div>
+          <div id='fourteen'> 14 </div>
+          <div id='fifteen'> 15 </div>
+          <div id='sixteen'> 16 </div>
 
         </section>
         <section className='sixth_row'>
-          <div className="row" id='seventeen'> 17 </div>
-          <div className="row" id='eighteen'> 18 </div>
-          <div className="row" id='nineteen'> 19 </div>
-          <div className="row" id='twenty'> 20 </div>
-          <div className="row" id='twenty-one'> 21 </div>
-          <div className="row" id='twenty-two'> 22 </div>
-          <div className="row" id='twenty-three'> 23 </div>
+          <div id='seventeen'> 17 </div>
+          <div id='eighteen'> 18 </div>
+          <div id='nineteen'> 19 </div>
+          <div id='twenty'> 20 </div>
+          <div id='twenty-one'> 21 </div>
+          <div id='twenty-two'> 22 </div>
+          <div id='twenty-three'> 23 </div>
         </section>
 
         <section className='seventh_row'>
  
-          <div className="row" id='twenty-four'> 24 </div>
+          <div id='twenty-four'> 24 </div>
         </section>
       </div>
     </>
