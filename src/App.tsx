9bbe0f01cdf0322b80ab9f187e36ce8e5708bc80
Elisabeth <elise.doisneau@gmail.com>
2024-11-28 20:51:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,327 @@
 
 import './App.css'
+import one from "./assets/five.png";
+import two from "./assets/two.png";
+import three from "./assets/three3.webp";
+import four from "./assets/four.png";
+import five from "./assets/one.png";
+import six from "./assets/six.svg";
+import seven from "./assets/seven.webp";
+import eight from "./assets/eight.webp";
+import nine from "./assets/nine.webp";
+import ten from "./assets/ten.svg";
+import eleven from "./assets/eleven.png";
+import twelve from "./assets/twelve.png";
+import thirteen from "./assets/thirteen.png";
+import fourteen from "./assets/fourteen.png";
+import fifteen from "./assets/fifteen.webp";
+import sixteen from "./assets/sixteen.webp";
+import seventeen from "./assets/seventeen.png";
+import eighteen from "./assets/eighteen.webp";
+import nineteen from "./assets/nineteen.webp";
+import twenty from "./assets/twenty.webp";
+import twentyone from "./assets/twentyone.svg";
+import twentytwo from "./assets/twentytwo.webp";
+import twentythree from "./assets/twentythree.webp";
+import twentyfour from "./assets/twentyfour.webp";
 
 function App() {
 
 
   return (
     <>
-      <div> 
+      <div>
         <section className='first_row'>
-          <div id='one'> 1 </div>
+          <div id='one'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={one} alt="one"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p>I love coding</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className='second_row'>
-          <div id='two'> 2 </div>
-          <div id='three'> 3 </div>
+          <div id='two'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={two} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='three'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={three} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> 
+          </div>
         </section>
         <section className='third_row'>
-          <div id='four'> 4 </div>
-          <div id='five'> 5 </div>
-          <div id='six'> 6 </div>
+          <div id='four'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={four} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='five'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={five} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='six'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={six} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className='fourth_row'>
-          <div id='seven'> 7 </div>
-          <div id='eight'> 8 </div>
-          <div id='nine'> 9 </div>
-          <div id='ten'> 10 </div>
+          <div id='seven'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={seven} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='eight'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={eight} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='nine'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={nine} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='ten'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={ten} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className='fifth_row'>
-          <div id='eleven'> 11 </div>
-          <div id='twelve'> 12 </div>
-          <div id='thirteen'> 13 </div>
-          <div id='fourteen'> 14 </div>
-          <div id='fifteen'> 15 </div>
-          <div id='sixteen'> 16 </div>
+          <div id='eleven'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={eleven} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='twelve'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twelve} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>  </div>
+          <div id='thirteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={thirteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='fourteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={fourteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>  </div>
+          <div id='fifteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={fifteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id='sixteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={sixteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
 
         </section>
         <section className='sixth_row'>
-          <div id='seventeen'> 17 </div>
-          <div id='eighteen'> 18 </div>
-          <div id='nineteen'> 19 </div>
-          <div id='twenty'> 20 </div>
-          <div id='twenty-one'> 21 </div>
-          <div id='twenty-two'> 22 </div>
-          <div id='twenty-three'> 23 </div>
+          <div id='seventeen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={seventeen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='eighteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={eighteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='nineteen'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={nineteen} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='twenty'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twenty} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='twenty-one'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twentyone} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='twenty-two'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twentytwo} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
+          <div id='twenty-three'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twentythree} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div> </div>
         </section>
 
         <section className='seventh_row'>
-          <div id='twenty-four'> 24 </div>
+          <div id='twenty-four'>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={twentyfour} alt="two"></img>
+                </div>
+                <div className="flip-card-back">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </>
